e54197b3e4cd62a117e0839d962d9fe79f71903d
ahmedEAmer <a7med_etoo@hotmail.com>
2020-06-10 21:16:11 +0200

    Hunt: 001 Setup Gitignore

---
+++ UE4_Project_Hunt/Source/UE4_Project_Hunt/UE4_Project_HuntGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE4_Project_HuntGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4_PROJECT_HUNT_API AUE4_Project_HuntGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
