092d5eef103e70167ab026664c78398214cd6a37
ahmedEAmer <a7med_etoo@hotmail.com>
2020-06-11 18:17:43 +0200

    Ahmed Ehab: 006 - Improve the Damage system now player can take damage from actors like (Fire)

---
+++ UE4_Project_Hunt/Source/UE4_Project_Hunt/Private/DamageActor.cpp
@@ -0,0 +1,58 @@
+ // Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DamageActor.h"
+#include "Kismet/GameplayStatics.h"
+#include "TimerManager.h"
+#include "Components/BoxComponent.h"
+#include "Particles/ParticleSystemComponent.h"
+
+// Sets default values
+ADamageActor::ADamageActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	
+	MyBoxComponent = CreateDefaultSubobject<UBoxComponent>("Damage Box");
+	MyBoxComponent->InitBoxExtent(BoxSize);
+	RootComponent = MyBoxComponent;
+
+	Particle = CreateDefaultSubobject<UPrimitiveComponent>("Particle");
+	Particle->SetRelativeLocation(FVector(0.f, 0.f, 0.f));
+	Particle->SetupAttachment(RootComponent);
+
+
+	MyBoxComponent->OnComponentBeginOverlap.AddDynamic(this, &ADamageActor::OnOverlapBegin);
+	MyBoxComponent->OnComponentEndOverlap.AddDynamic(this, &ADamageActor::OnOverlapEnd);
+
+	bCanBeDamaged = false;
+
+}
+
+void ADamageActor::OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor,
+	class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+	const FHitResult& SweepResult)
+{
+	if ( (OtherActor != nullptr) && (OtherActor != this) && (OtherComp != nullptr) )
+	{
+		bCanBeDamaged = true;
+		MyCharacter = Cast<AActor>(OtherActor);
+		MyHit = SweepResult;
+		GetWorldTimerManager().SetTimer(DamageTimerHandle, this, &ADamageActor::ApplyFireDamage, 2.2f,
+		true, 0.0f);
+	}
+}
+void ADamageActor::OnOverlapEnd(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor,
+	class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	bCanBeDamaged = false;
+	GetWorldTimerManager().ClearTimer(DamageTimerHandle);
+}
+
+void ADamageActor::ApplyDamage()
+{
+	if (bCanBeDamaged)
+	{
+		UGameplayStatics::ApplyPointDamage(MyCharacter, DamageAmont, GetActorLocation(),
+		MyHit, nullptr, this, FireDamageType)
+	}
+}
---
+++ UE4_Project_Hunt/Source/UE4_Project_Hunt/Public/DamageActor.h
@@ -0,0 +1,60 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "DamageActor.generated.h"
+
+UCLASS()
+class UE4_PROJECT_HUNT_API ADamageActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ADamageActor();
+
+public:
+
+	UPROPERTY(EditAnywhere)
+	UParticleSystemComponent* Particle;
+
+	UPROPERTY(EditAnywhere)
+	UBoxComponent* MyBoxComponent;
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<UDamageType> FireDamageType;
+
+	UPROPERTY(EditAnywhere)
+	AActor* MyCharacter;
+
+	UPROPERTY(EditAnywhere)
+	FHitResult MyHit;
+
+	UPROPERTY(EditAnywhere)
+	FVector BoxSize = FVector(50.f, 50.f, 50.f);
+
+	UPROPERTY(EditAnywhere, Category = "Damage")
+	float DamageAmont = 20.f;
+
+	bool bCanApplyDamage;
+	FTimerHandle DamageTimerHandle;
+
+	// declare component overlap begin function
+	UFUNCTION()
+		void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor,
+		class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+		const FHitResult& SweepResult);
+
+	// declare component overlap end function
+	UFUNCTION()
+		void OnOverlapEnd(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, 
+		class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	UFUNCTION()
+		void ApplyDamage();
+
+	
+
+};
