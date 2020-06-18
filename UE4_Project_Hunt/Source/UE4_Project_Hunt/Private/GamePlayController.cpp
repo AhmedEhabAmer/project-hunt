faf83f8a149ef72023e12bccf6b05106273ea291
DESKTOP-CS5T0QE\a7med <a7med_Etoo@hotmail.com>
2020-06-18 17:57:26 +0200

    Ahmed Ehab: 017 - Setup the AI and start the patrol points system

--- UE4_Project_Hunt/Source/UE4_Project_Hunt/Private/GamePlayController.cpp
+++ UE4_Project_Hunt/Source/UE4_Project_Hunt/Private/GamePlayController.cpp
@@ -1,8 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include "GamePlayController.h"
 #include "Kismet/GamePlayStatics.h"
 #include "DrawDebugHelpers.h"
-#include "GamePlayController.h"
 
 AGamePlayController::AGamePlayController()
 {
--- UE4_Project_Hunt/Source/UE4_Project_Hunt/UE4_Project_HuntGameModeBase.cpp
+++ UE4_Project_Hunt/Source/UE4_Project_Hunt/UE4_Project_HuntGameModeBase.cpp
@@ -2,4 +2,4 @@
 
 
 #include "UE4_Project_HuntGameModeBase.h"
-
+#include "GameplayTaskOwnerInterface.h"
